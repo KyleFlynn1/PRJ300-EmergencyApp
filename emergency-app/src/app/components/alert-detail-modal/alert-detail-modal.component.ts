cf98f19c111eb0953c3f449b044f09ab60ca842e
KyleFlyn <158482875+KyleFlynn1@users.noreply.github.com>
2025-11-16 23:33:10 +0000

    Created the component for detailed alerts

---
+++ emergency-app/src/app/components/alert-detail-modal/alert-detail-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-detail-modal',
+  templateUrl: './alert-detail-modal.component.html',
+  styleUrls: ['./alert-detail-modal.component.scss'],
+})
+export class AlertDetailModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
