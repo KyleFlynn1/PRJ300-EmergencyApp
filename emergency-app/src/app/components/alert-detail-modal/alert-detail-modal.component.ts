f6379b6b668156a5979baccf558046aa6e91b4e9
KyleFlyn <158482875+KyleFlynn1@users.noreply.github.com>
2025-11-16 23:49:06 +0000

    Added the modal opening on clicking cards and basic information

--- emergency-app/src/app/components/alert-detail-modal/alert-detail-modal.component.ts
+++ emergency-app/src/app/components/alert-detail-modal/alert-detail-modal.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert-detail-modal',
   templateUrl: './alert-detail-modal.component.html',
   styleUrls: ['./alert-detail-modal.component.scss'],
+  imports: [IonicModule],
+  standalone: true,
 })
 export class AlertDetailModalComponent  implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {}
 
+  closeDetailedView() {
+    this.modalController.dismiss(null, 'cancel');
+  }
 }
--- emergency-app/src/app/pages/home/home.page.ts
+++ emergency-app/src/app/pages/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReportModalComponent } from 'src/app/components/report-modal/report-modal.component';
+import { AlertDetailModalComponent } from 'src/app/components/alert-detail-modal/alert-detail-modal.component';
 import { ModalController } from '@ionic/angular';
 import { IonicModule } from '@ionic/angular';
 import { Alert } from 'src/app/services/alerts/alert';
@@ -43,6 +44,20 @@ export class HomePage implements OnInit {
     const { data } = await modal.onDidDismiss();
     if (data) {
       console.log('Report got succesfully:', data);
+    }
+  }
+
+  async openAlertDetailModal() {
+    const modal = await this.modalController.create({
+      component: AlertDetailModalComponent,
+      cssClass: 'floating-modal',
+      backdropDismiss: true,
+      showBackdrop: true
+    });
+    await modal.present();
+    const { data } = await modal.onDidDismiss();
+    if (data) {
+      console.log('Alert detail modal dismissed with data:', data);
     }
   }
 
