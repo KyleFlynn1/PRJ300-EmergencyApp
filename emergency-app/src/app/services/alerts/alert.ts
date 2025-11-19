181f77d681691c8b5c004d69c94e4a4f66a0bf00
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-11-19 15:29:21 +0000

    made it work for the forms

--- emergency-app/src/app/services/alerts/alert.ts
+++ emergency-app/src/app/services/alerts/alert.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Report } from 'src/app/interfaces/report.interface';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +11,20 @@ export class Alert {
 
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:3000/api/v1/alert';
+  private alerts: Report[] = [];
+  private alertsSubject = new BehaviorSubject<Report[]>(this.alerts);
 
 
   getAlerts(): Observable<Report[]> {
     return this.http.get<Report[]>(this.apiUrl);
   }
+
   addAlert(report: Report): Observable<Report> {
-    return this.http.post<Report>(this.apiUrl, report);
+  return this.http.post<Report>(this.apiUrl, report).pipe(
+    tap((newAlert) => {
+      this.alerts = [newAlert, ...this.alerts];
+      this.alertsSubject.next(this.alerts);
+      })
+    );
   }
 }
