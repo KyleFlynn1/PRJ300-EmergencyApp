b443a8780b636be809dc0d2fe2698a539ef73f6f
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-11-27 00:10:06 +0000

    changed interface for the ergency and added the deploy to androd fixed the config

--- emergency-app/src/app/interfaces/report.interface.ts
+++ emergency-app/src/app/interfaces/report.interface.ts
@@ -1,5 +1,5 @@
 export interface Report {
-  severity: 'Low' | 'Moderate' | 'High' | 'Info';
+  severity: 'Low' | 'Moderate' | 'High' | 'Info' | 'Urgent';
   category: string;
   notes?: string;
   timestamp: string;
