13f0ebbfb769954eabfa51ec3b07c6ffacd68c02
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-12-01 18:51:56 +0000

    added empty service for defib

---
+++ emergency-app/src/app/services/defib/defib.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Defib as DefibInterface } from 'src/app/interfaces/defib.interface';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Defib {
+
+  
+}
