2831e106bf0dc41401c371a6284f03ffaac0f5f9
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-12-01 19:29:37 +0000

    added delete button, not properly styled

--- emergency-app/src/app/components/alert-detail-modal/alert-detail-modal.component.ts
+++ emergency-app/src/app/components/alert-detail-modal/alert-detail-modal.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { Icon } from 'ionicons/dist/types/components/icon/icon';
 import { Report } from 'src/app/interfaces/report.interface';
+import { Alert } from 'src/app/services/alerts/alert';
 import { getAlertSeverityColor, getIcon } from 'src/app/utils/modalUtil';
 import Map from 'ol/Map';
 import View from 'ol/View';
@@ -27,13 +28,15 @@ export class AlertDetailModalComponent  implements OnInit, AfterViewInit {
   getAlertSeverityColor = getAlertSeverityColor;
   getIcon = getIcon;
   
+  
   @Input() isNativeModal : boolean = false;
   @Input() alert?: Report;
   
   map?: Map;
 
   constructor(
-    private modalController: ModalController
+    private modalController: ModalController,
+    private alertService: Alert
   ) {}
 
   ngOnInit() {
@@ -88,9 +91,27 @@ export class AlertDetailModalComponent  implements OnInit, AfterViewInit {
   }
 
   // Close modal
-  closeDetailedView() {
+  async closeDetailedView() {
     if (!this.isNativeModal) {
-      this.modalController.dismiss();
+      //console.warn('yes closeDetailedView called in non-native modal context');
+      await this.modalController.dismiss();
+    } else {
+      //console.warn('no closeDetailedView called in native modal context');
+      // If using native modal, navigate to home
+      window.location.href = '/home';
+    }
+  }
+  deleteAlert() {
+    if (this.alert && this.alert._id) {
+      this.alertService.deleteAlert(this.alert._id).subscribe({
+        next: () => {
+          console.log('Alert deleted successfully');
+          this.closeDetailedView();
+        },
+        error: (err) => {
+          console.error('Error deleting alert:', err);
+        }
+      });
     }
   }
 }
