fb03f2578d20b8a0b2f9a5febbe96d7b808ed8e8
KyleFlynn1 <s00251601@atu.ie>
2025-12-01 18:12:57 +0000

    Made Nav Bigger
    Styling on filters and nav along with radio buttons on defib form

--- emergency-app/src/app/app.component.ts
+++ emergency-app/src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  imports: [IonicModule, RouterLink],
+  imports: [IonicModule, RouterLink, CommonModule],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showBottomNav = true;
+  activeTab = 'home';
   
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Set initial active tab
+    this.updateActiveTab(this.router.url);
+
+    // Listen to route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.updateActiveTab(event.urlAfterRedirects);
+    });
+  }
+
+  updateActiveTab(url: string) {
+    if (url.includes('/home')) {
+      this.activeTab = 'home';
+    } else if (url.includes('/alerts')) {
+      this.activeTab = 'alerts';
+    } else if (url.includes('/map')) {
+      this.activeTab = 'map';
+    } else if (url.includes('/defibilators')) {
+      this.activeTab = 'defib';
+    }
+  }
 }
