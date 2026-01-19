8086d946296a9d5654ce3b74485f30e157a03646
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2026-01-19 20:16:37 +0000

    failed to add

--- emergency-app/src/app/app.component.ts
+++ emergency-app/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonIcon } from '@ionic/angular/standalone';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { PushService } from './services/push/push';
+import { PushNotifications } from '@capacitor/push-notifications';
+import { Capacitor } from '@capacitor/core';
+
 
 @Component({
   selector: 'app-root',
@@ -13,4 +16,24 @@ export class AppComponent {
   constructor(private pushService: PushService) {
   this.pushService.init();
 }
+ngOnInit() {
+  if (Capacitor.isNativePlatform()) {
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        PushNotifications.register();
+      }
+    });
+
+    PushNotifications.addListener('registration', token => {
+      console.log('📱 FCM TOKEN:', token.value);
+      alert('FCM Token:\n' + token.value); // TEMP: easy copy
+    });
+
+    PushNotifications.addListener('registrationError', err => {
+      console.error('Registration error:', err);
+    });
+  }
 }
+}
+
+
